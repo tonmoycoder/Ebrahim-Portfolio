9b48c01106d9ad8cf812321d51f3df48185dc5ee
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-01-07 18:27:48 +0600

    skill done

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,10 +1,165 @@
-import React from 'react'
-import './Services.css'
+import React, { useState } from 'react';
+import './Services.css';
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+  const [toggleState, setToggleState] = useState(0);
 
-export default Services
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  return (
+    <section className="services section" id="services">
+      <h2 className="section__title">Services</h2>
+      <span className="section__subtitle">Services I provide</span>
+
+      <div className="services__container container grid">
+        <div className="services__content">
+          <div>
+            <i className="uil uil-web-grid services__icon"></i>
+            <h3 className="services__title">
+              Product <br /> Designer
+            </h3>
+          </div>
+
+          <span className="services__button" onClick={() => toggleTab(1)}>
+            View More <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+              <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+              <h3 className="services_modal-title">Product Designer</h3>
+              <p className="services__modal-description">
+                Services with more than 5 years of experience. Lorem ipsum dolor{' '}
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I develop user Interface</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">web page development</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I create UX element Interactions</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I Re-branding company</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I Design and mockups of products for companies
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="services__content">
+          <div>
+            <i className="uil uil-arrow services__icon"></i>
+            <h3 className="services__title">
+              UI/UI <br />
+              Designer
+            </h3>
+          </div>
+
+          <span className="services__button" onClick={() => toggleTab(2)}>
+            View More <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+              <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+              <h3 className="services_modal-title">UI/UI Designer</h3>
+              <p className="services__modal-description">
+                Services with more than 5 years of experience. Lorem ipsum dolor{' '}
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I develop user Interface</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">web page development</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I create UX element Interactions</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I Re-branding company</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I Design and mockups of products for companies
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="services__content">
+          <div>
+            <i className="uil uil-edit services__icon"></i>
+            <h3 className="services__title">
+              Designer <br />
+              Developer
+            </h3>
+          </div>
+
+          <span className="services__button" onClick={() => toggleTab(3)}>
+            View More <i className="uil uil-arrow-right services__button-icon"></i>
+          </span>
+
+          <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+              <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+
+              <h3 className="services_modal-title">Designer Developer</h3>
+              <p className="services__modal-description">
+                Services with more than 5 years of experience. Lorem ipsum dolor{' '}
+              </p>
+              <ul className="services__modal-services grid">
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I develop user Interface</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">web page development</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I create UX element Interactions</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">I Re-branding company</p>
+                </li>
+                <li className="services__modal-service">
+                  <i className="uil uil-check-circle service__modal-icon"></i>
+                  <p className="services__modal-info">
+                    I Design and mockups of products for companies
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Qualification.css'
+
+const Qualification = () => {
+    return (
+        <div>
+            a
+        </div>
+    );
+};
+
+export default Qualification;
