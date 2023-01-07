ac4c8cbc2314a44ff7e7a5b43698ed31319c74c5
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-01-08 01:22:21 +0600

    contact  done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
 import Header from './components/header/Header';
 import Home from './components/Home/Home';
 import Qualification from './components/qualification/Qualification';
@@ -19,6 +20,7 @@ function App() {
         <Services></Services>
         <Qualification></Qualification>
         <Testimonial></Testimonial>
+        <Contact></Contact>
       </main>
     </>
   );
